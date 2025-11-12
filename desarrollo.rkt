c8699fc2e4791b386324ba4641a98fc21c6e1f5f
AvalosJorge00009322 <00009322@uca.edu.sv>
2025-11-12 16:07:18 -0600

    feat: enumerar ejercicios

--- desarrollo.rkt
+++ desarrollo.rkt
@@ -7,7 +7,9 @@
            lst)))
 
 ;; Prueba
-(contar-positivos '(3 -2 7 0 -5 9))
+(printf "Ejercicio 1: ~a\n"
+        (contar-positivos '(3 -2 7 0 -5 9)))
+
 
 ;; Ejercicio 2
 (define (cuadrados-pares lst)
@@ -15,7 +17,9 @@
        (filter even? lst)))
 
 ;; Prueba
-(cuadrados-pares '(1 2 3 4 5 6 7 8))
+(printf "Ejercicio 2: ~a\n"
+        (cuadrados-pares '(1 2 3 4 5 6 7 8)))
+
 
 ;; Ejercicio 3
 (define (factorial n)
@@ -24,36 +28,45 @@
       (* n (factorial (sub1 n)))))
 
 ;; Prueba
-(factorial 5)
+(printf "Ejercicio 3: ~a\n"
+        (factorial 5))
+
 
 ;; Ejercicio 4
-
 (define (cubos lst)
   (map (lambda (x) (* x x x))
        lst))
 
 ;; Prueba
-(cubos '(2 3 4))
+(printf "Ejercicio 4: ~a\n"
+        (cubos '(2 3 4)))
 
-;;Ejercicio 5
+
+;; Ejercicio 5
 (define (sumar-impares lst)
   (foldl +          ; función de acumulación (suma)
          0          ; valor inicial
          (filter odd? lst)))  ; nos quedamos solo con los impares
 
 ;; Prueba
-(sumar-impares '(1 2 3 4 5 6 7))  
+(printf "Ejercicio 5: ~a\n"
+        (sumar-impares '(1 2 3 4 5 6 7)))
 
-;;Ejercicio 6
+
+;; Ejercicio 6
 (define (contiene-negativos? lst)
   (ormap (lambda (x) (< x 0))  ; ¿es negativo?
          lst))
 
 ;; Pruebas
-(contiene-negativos? '(5 9 -3 2))  ; t
-(contiene-negativos? '(1 2 3 4))   ; f
+(printf "Ejercicio 6 (lista con negativo): ~a\n"
+        (contiene-negativos? '(5 9 -3 2)))  ; #t
 
-;;Ejercicio 7
+(printf "Ejercicio 6 (lista sin negativo): ~a\n"
+        (contiene-negativos? '(1 2 3 4)))   ; #f
+
+
+;; Ejercicio 7
 (define (suma-acumulada lst)
   (reverse
    (foldl (lambda (x acc)
@@ -65,16 +78,21 @@
           lst)))
 
 ;; Prueba
-(suma-acumulada '(1 2 3 4))  
+(printf "Ejercicio 7: ~a\n"
+        (suma-acumulada '(1 2 3 4)))
 
-;;Ejercico 8
+
+;; Ejercicio 8
 (define (concatenar-cadenas lst)
-  (foldl string-append  ; cómo acumulamos: pegando cadenas
-         ""             ; cadena inicial vacía
+  (foldl (lambda (elem acc)
+           (string-append acc elem)) ; respeta el orden de la lista
+         ""                          ; cadena inicial vacía
          lst))
 
 ;; Prueba
-(concatenar-cadenas '("Hola" " " "Mundo"))
+(printf "Ejercicio 8: ~a\n"
+        (concatenar-cadenas '("Hola" " " "Mundo")))
+
 
 ;; Ejercicio 9
 (define (doble-mayores-que-5 lst)
@@ -83,7 +101,9 @@
                lst)))
 
 ;; Prueba
-(doble-mayores-que-5 '(3 6 8 2 10))
+(printf "Ejercicio 9: ~a\n"
+        (doble-mayores-que-5 '(3 6 8 2 10)))
+
 
 ;; Ejercicio 10
 (define (invertir-lista lst)
@@ -93,16 +113,21 @@
          lst))
 
 ;; Prueba
-(invertir-lista '(1 2 3 4))
+(printf "Ejercicio 10: ~a\n"
+        (invertir-lista '(1 2 3 4)))
 
-;;Ejercicio 11
+
+;; Ejercicio 11
 (define (cuadrado x)
   (* x x))
+
 (define (aplicar-a-lista f lst)
   (map f lst))
 
 ;; Prueba
-(aplicar-a-lista cuadrado '(1 2 3 4))
+(printf "Ejercicio 11: ~a\n"
+        (aplicar-a-lista cuadrado '(1 2 3 4)))
+
 
 ;; Ejercicio 12
 (define (promedio-mayores-que-5 lst)
@@ -112,9 +137,9 @@
          ;; sumamos con foldl
          [suma (foldl + 0 mayores-real)])
     (if (null? mayores-real)
-        0                              
+        0
         (/ suma (length mayores-real))))) 
 
 ;; Prueba
-(promedio-mayores-que-5 '(3 8 10 4 9 2 7))
-
+(printf "Ejercicio 12: ~a\n"
+        (promedio-mayores-que-5 '(3 8 10 4 9 2 7)))
